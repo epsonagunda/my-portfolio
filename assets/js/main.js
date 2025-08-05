6a6cbb02d582492bd6b466aa01c68996b5ef31dc
epson agunda <122625227+epsonagunda@users.noreply.github.com>
2025-08-05 12:03:15 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
+const toggle = document.getElementById('nav-toggle');
+const nav = document.getElementById('nav-menu');
+
+if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('show');
+    });
+}
 
